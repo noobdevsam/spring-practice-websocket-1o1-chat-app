b1011f41ea1bdf70d2c1784cf077b63b3d678ad6
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-19 00:36:30 +0600

    add WebSocketConfig implementation for configuring WebSocket message broker and STOMP endpoints

---
+++ src/main/java/com/example/springpracticewebsocket1o1chatapp/configs/WebSocketConfig.java
@@ -0,0 +1,46 @@
+package com.example.springpracticewebsocket1o1chatapp.configs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.DefaultContentTypeResolver;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/user");
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.setUserDestinationPrefix("/user");
+    }
+
+    @Override
+    public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
+        var resolver = new DefaultContentTypeResolver();
+        var converter = new MappingJackson2MessageConverter();
+
+        resolver.setDefaultMimeType(MimeTypeUtils.APPLICATION_JSON);
+
+        converter.setObjectMapper(new ObjectMapper());
+        converter.setContentTypeResolver(resolver);
+
+        messageConverters.add(converter);
+
+        return false; // Return false to indicate that we are not adding any additional converters
+    }
+}
