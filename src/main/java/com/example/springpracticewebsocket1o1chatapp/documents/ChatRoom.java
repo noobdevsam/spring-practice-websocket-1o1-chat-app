08e8f191513484a7ecd3f7a356bf77f74e503507
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-18 21:30:52 +0600

    add builder pattern and equals/hashCode methods to ChatRoom class

--- src/main/java/com/example/springpracticewebsocket1o1chatapp/documents/ChatRoom.java
+++ src/main/java/com/example/springpracticewebsocket1o1chatapp/documents/ChatRoom.java
@@ -1,7 +1,11 @@
 package com.example.springpracticewebsocket1o1chatapp.documents;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Objects;
+
+@Document
 public class ChatRoom {
 
     @Id
@@ -9,6 +13,28 @@ public class ChatRoom {
     private String chatId;
     private String senderId;
     private String recipientId;
+
+    public ChatRoom() {
+    }
+
+    public ChatRoom(String id, String chatId, String senderId, String recipientId) {
+        this.id = id;
+        this.chatId = chatId;
+        this.senderId = senderId;
+        this.recipientId = recipientId;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (!(o instanceof ChatRoom chatRoom)) return false;
+
+        return Objects.equals(id, chatRoom.id) && Objects.equals(chatId, chatRoom.chatId) && Objects.equals(senderId, chatRoom.senderId) && Objects.equals(recipientId, chatRoom.recipientId);
+    }
+
 
     public String getId() {
         return id;
@@ -40,5 +66,45 @@ public class ChatRoom {
 
     public void setRecipientId(String recipientId) {
         this.recipientId = recipientId;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(id);
+        result = 31 * result + Objects.hashCode(chatId);
+        result = 31 * result + Objects.hashCode(senderId);
+        result = 31 * result + Objects.hashCode(recipientId);
+        return result;
+    }
+
+    public static class Builder {
+        private String id;
+        private String chatId;
+        private String senderId;
+        private String recipientId;
+
+        public Builder id(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder chatId(String chatId) {
+            this.chatId = chatId;
+            return this;
+        }
+
+        public Builder senderId(String senderId) {
+            this.senderId = senderId;
+            return this;
+        }
+
+        public Builder recipientId(String recipientId) {
+            this.recipientId = recipientId;
+            return this;
+        }
+
+        public ChatRoom build() {
+            return new ChatRoom(id, chatId, senderId, recipientId);
+        }
     }
 }
