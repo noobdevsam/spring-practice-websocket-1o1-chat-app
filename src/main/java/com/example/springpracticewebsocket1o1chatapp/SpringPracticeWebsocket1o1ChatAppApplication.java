3c8634d4ef972a7e8351a11d0f063c241ed79d44
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-18 21:13:48 +0600

    initial commit

---
+++ src/main/java/com/example/springpracticewebsocket1o1chatapp/SpringPracticeWebsocket1o1ChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springpracticewebsocket1o1chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeWebsocket1o1ChatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPracticeWebsocket1o1ChatAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springpracticewebsocket1o1chatapp/SpringPracticeWebsocket1o1ChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springpracticewebsocket1o1chatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeWebsocket1o1ChatAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
